fcabaa4928ec0c3637c18b8b9f4fcc87e7710287
lalinda <nawinjith96@gmail.com>
2019-06-19 08:55:48 +0530

    shop controllers and init views set

--- application/controllers/Shop.php
+++ application/controllers/Shop.php
@@ -5,8 +5,26 @@ class Shop extends CI_Controller {
 
 	public function index()
 	{
-		
 		$this->load->view('shop/shopmain.php');
-	
+	}
+
+	public function add_order()
+	{
+		$this->load->view('shop/shopaddorder.php');
+	}
+
+	public function sales_history()
+	{
+		$this->load->view('shop/shopsaleshistory.php');
+	}
+
+	public function make_payment()
+	{
+		$this->load->view('shop/shopmakepayment.php');
+	}
+
+	public function sales_predict()
+	{
+		$this->load->view('shop/shopsalespredict.php');
 	}
 }
---
+++ application/views/shop/shopaddorder.php
@@ -0,0 +1,1 @@
+<h1>add  new order module here and link payment </h1>
